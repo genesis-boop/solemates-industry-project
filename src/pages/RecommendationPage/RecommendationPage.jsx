1528ce8e29259f3f77505297285a5e479cae7d94
unknown <genesisgithub@gmail.com>
2024-04-05 10:34:05 -0700

    Recommendation Card

---
+++ src/pages/RecommendationPage/RecommendationPage.jsx
@@ -0,0 +1,7 @@
+import RecommendationCard from "../../components/RecommendationCard/RecommendationCard";
+
+function RecommendationPage() {
+  return <RecommendationCard />;
+}
+
+export default RecommendationPage;
