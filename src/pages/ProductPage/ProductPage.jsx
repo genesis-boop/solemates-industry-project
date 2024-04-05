4dcd34a629b0323e274a25299e72fbf5a9288dd0
mark-wytiu <mark.wytiu@hotmail.com>
2024-04-05 11:47:01 -0700

    added comments component

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -2,12 +2,16 @@ import "./ProductPage.scss";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import SimiliarItems from "../../components/SimiliarItems/SimiliarItems";
+import Comments from "../../components/Comments/Comments";
+
+
 function ProductPage() {
   return (
     <main className="product-page">
       <SearchBar />
       <ProductCard />
       <SimiliarItems />
+      <Comments />
     </main>
   );
 }
