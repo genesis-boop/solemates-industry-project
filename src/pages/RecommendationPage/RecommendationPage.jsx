217c279ae5e57b9277ef2d7accc160f9be111847
mark wytiu <markwytiu@Macky-macbook-pro.local>
2024-04-04 23:07:37 -0700

    added div in rec-page

--- src/pages/RecommendationPage/RecommendationPage.jsx
+++ src/pages/RecommendationPage/RecommendationPage.jsx
@@ -1,10 +1,14 @@
 import RecommendationCard from "../../components/RecommendationCard/RecommendationCard"
 
-function RecommendationPage () {
+function RecommendationPage() {
     return (
         <div className="rec-page">
 
-
+            <div className="rec-page-feet__details">
+                <p className="rec-page-feet__text">High Arch</p>
+                <p className="rec-page-feet__text">Medium Volume</p>
+                <p className="rec-page-feet__text">Wide Width</p>
+            </div>
             <h2>Perfect Fit Only For You</h2>
             <RecommendationCard />
         </div>
