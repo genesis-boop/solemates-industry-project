dbfa3023d7d6e42ada40e13e56acd71a25f252e7
mark wytiu <markwytiu@Macky-macbook-pro.local>
2024-04-04 22:44:58 -0700

    import rec-card into and added rec-page component

---
+++ src/pages/RecommendationPage/RecommendationPage.jsx
@@ -0,0 +1,12 @@
+import RecommendationCard from "../../components/RecommendationCard/RecommendationCard"
+
+function RecommendationPage () {
+    return (
+        <div className="rec-page">
+
+        
+            <h2>Perfect Fit Only For You</h2>
+            <RecommendationCard />
+        </div>
+    )
+}
